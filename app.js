e3dd74220bd5fc73ebe09c5783ffd8bf6b931dfa
davicoll <collares.fr@gmail.com>
2023-01-03 21:33:40 +0100

    addsauce ok

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const app = express();
 
 const userRoutes = require("./routes/user");
-//const sauceRoutes = require("./routes/sauces");
+const sauceRoutes = require("./routes/sauces");
 
 mongoose
   .connect(
@@ -24,13 +24,12 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
   );
-  next();
 });
 
 app.use(express.json());
 
 app.use("/api/auth/", userRoutes);
-//app.use("/api/sauces/", sauceRoutes);
+app.use("/api/sauces/", sauceRoutes);
 //app.use("/images", express.static(path.join(__dirname, "images")));
 
 module.exports = app;
--- models/sauces.js
+++ models/sauces.js
@@ -1,15 +1,17 @@
 const mongoose = require("mongoose");
 
-const thingSchema = mongoose.Schema({
-  title: { type: String, required: true },
+const sauceSchema = mongoose.Schema({
+  name: { type: String, required: true },
   manufacturer: { type: String, required: true },
   description: { type: String, required: true },
-  mainPepper: { type: Number, required: true },
+  mainPepper: { type: String },
   imageUrl: { type: String, required: true },
   userId: { type: String, required: true },
   heat: { type: Number, required: true },
   likes: { type: Number },
   dislikes: { type: Number },
+  usersLiked: ["String <userId>"],
+  usersDisliked: ["String <userId>"],
 });
 
-module.exports = mongoose.model("Thing", thingSchema);
+module.exports = mongoose.model("Sauce", sauceSchema);
