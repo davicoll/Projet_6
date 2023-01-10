9466dad425eb063f5afa1d5389b26f06289e2248
davicoll <collares.fr@gmail.com>
2023-01-10 10:46:19 +0100

    probably almost finished

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -1,5 +1,6 @@
 const Sauce = require("../models/sauces");
 const fs = require("fs");
+const { Console } = require("console");
 
 exports.createSauce = (req, res, next) => {
   const sauceObject = JSON.parse(req.body.sauce);
@@ -53,7 +54,7 @@ exports.updateSauce = (req, res, next) => {
   Sauce.findOne({ _id: req.params.id })
     .then((sauce) => {
       if (sauce.userId != req.auth.userId) {
-        res.status(401).json({ message: "Not authorized" });
+        res.status(403).json({ message: "Not authorized" });
       } else {
         Sauce.updateOne(
           { _id: req.params.id },
@@ -72,7 +73,7 @@ exports.deleteSauce = (req, res, next) => {
   Sauce.findOne({ _id: req.params.id })
     .then((sauce) => {
       if (sauce.userId != req.auth.userId) {
-        res.status(401).json({ message: "Not authorized" });
+        res.status(403).json({ message: "Not authorized" });
       } else {
         const filename = sauce.imageUrl.split("/images/")[1];
         fs.unlink(`images/${filename}`, () => {
@@ -89,4 +90,31 @@ exports.deleteSauce = (req, res, next) => {
     });
 };
 
-exports.likeSauce = (req, res, next) => {};
+exports.likeSauce = (req, res, next) => {
+  Sauce.findOne({ _id: req.params.id }).then((sauce) => {
+    var indexL = sauce.usersLiked.indexOf(req.auth.userId);
+    var indexD = sauce.usersDisliked.indexOf(req.auth.userId);
+
+    if (req.body.like === 1 && indexL < 0) {
+      sauce.usersLiked.push(req.auth.userId);
+    } else if (req.body.like === -1 && indexD < 0) {
+      sauce.usersDisliked.push(req.auth.userId);
+    } else if (req.body.like === 0) {
+      if (indexD >= 0) {
+        sauce.usersDisliked.splice(indexD, 1);
+      } else if (indexL >= 0) {
+        sauce.usersLiked.splice(indexL, 1);
+      }
+    }
+    sauce.likes = parseInt(sauce.usersLiked.length);
+    sauce.dislikes = parseInt(sauce.usersDisliked.length);
+    sauce
+      .save()
+      .then(() => {
+        res.status(201).json({ message: "like enregistré !" });
+      })
+      .catch((error) => {
+        res.status(400).json({ error });
+      });
+  });
+};
